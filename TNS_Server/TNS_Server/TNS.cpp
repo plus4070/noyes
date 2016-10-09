9842bbe1f96a2555490871b9d2fb3af97802467c
DESKTOP-9OVJH4S\JunYoung <plus4070@gmail.com>
2016-10-09 17:52:33 +0900

    #2 TNSController 객체 추가, TCPSocket 서버 제거

--- TNS_Server/TNS_Server/TNS.cpp
+++ TNS_Server/TNS_Server/TNS.cpp
@@ -1,11 +1,12 @@
 #include "TNS.h"
+#include "TNSController.h"
 
 int main() {
 	// 서버 인스턴트
-	TCPSocket server;
+	TNSController server;
 
 	// 서버시작
-	server.StartServer();
+	server.startTNSServer();
 
 	return 0;
 }
