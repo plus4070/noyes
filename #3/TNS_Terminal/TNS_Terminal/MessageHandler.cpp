d7b2f2f2c3a9922fc76f0b8eeea296928cdec4f5
오준영 <plus4070@gmail.com>
2016-11-02 23:03:01 +0900

    MessageHandler.cpp 들여쓰기 정리

--- #3/TNS_Terminal/TNS_Terminal/MessageHandler.cpp
+++ #3/TNS_Terminal/TNS_Terminal/MessageHandler.cpp
@@ -8,19 +8,18 @@ MessageHandler::~MessageHandler()
 {
 }
 
-void				MessageHandler::setPDDNode(PDD_NODE *PN) {
+void MessageHandler::setPDDNode(PDD_NODE *PN) {
 	memset(PN, 0, sizeof(PDD_NODE));
 }
 
 
-void				MessageHandler::addDataToNode(PPDD_NODE PN, PDD_DATA PD) {
+void MessageHandler::addDataToNode(PPDD_NODE PN, PDD_DATA PD) {
 	memcpy(&(PN->PDD_DATA[PN->PDD_HEADER.NUMBER_OF_PARTICIPANT]), &PD, sizeof(PDD_DATA));
 	PN->PDD_HEADER.NUMBER_OF_PARTICIPANT++;
 }
 
-bool				MessageHandler::isPacketAvailable(PDD_NODE *PN) {
-	switch (PN->PDD_HEADER.MESSAGE_TYPE)
-	{
+bool MessageHandler::isPacketAvailable(PDD_NODE *PN) {
+	switch (PN->PDD_HEADER.MESSAGE_TYPE) {
 	case MESSAGE_TYPE_MODIFY:
 	case MESSAGE_TYPE_REMOVE:
 	case MESSAGE_TYPE_SAVE:
@@ -32,22 +31,22 @@ bool				MessageHandler::isPacketAvailable(PDD_NODE *PN) {
 	}
 }
 
-void			MessageHandler::setParticipantNumber(PDD_NODE *PN, int num) {
+void MessageHandler::setParticipantNumber(PDD_NODE *PN, int num) {
 	PN->PDD_HEADER.NUMBER_OF_PARTICIPANT = num;
 }
 
-void			MessageHandler::setMessageTypeTopicNotExist(PDD_NODE *PN) {
+void MessageHandler::setMessageTypeTopicNotExist(PDD_NODE *PN) {
 	PN->PDD_HEADER.MESSAGE_TYPE = MESSAGE_TYPE_NOTEXIST;
 }
 
-void			MessageHandler::setMessageTypeProcessDone(PDD_NODE *PN) {
+void MessageHandler::setMessageTypeProcessDone(PDD_NODE *PN) {
 	PN->PDD_HEADER.MESSAGE_TYPE += MESSAGE_OPTION_PLUS_DONE;
 }
 
-void			MessageHandler::setMessageType(PDD_NODE *PN, int type) {
+void MessageHandler::setMessageType(PDD_NODE *PN, int type) {
 	PN->PDD_HEADER.MESSAGE_TYPE = type;
 }
 
-void			MessageHandler::copyDatagram(PDD_NODE * oldNode, PDD_NODE * newNode) {
+void MessageHandler::copyDatagram(PDD_NODE * oldNode, PDD_NODE * newNode) {
 	memcpy(newNode, oldNode, sizeof(PDD_NODE));
 }
