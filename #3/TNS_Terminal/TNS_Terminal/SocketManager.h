bd07f802e9d93324253e1c0417f86c4a3fc3c904
오준영 <plus4070@gmail.com>
2016-11-02 22:51:12 +0900

    SocketManager.h 들여쓰기 정리

--- #3/TNS_Terminal/TNS_Terminal/SocketManager.h
+++ #3/TNS_Terminal/TNS_Terminal/SocketManager.h
@@ -14,40 +14,32 @@ public:
 	SocketManager();
 	~SocketManager();
 
-	CRITICAL_SECTION				cs;
-	deque<pair<IN_ADDR, PDD_NODE>>	recvData;
+	CRITICAL_SECTION cs;
+	deque<pair<IN_ADDR, PDD_NODE>> recvData;
 
-	void			 				setCriticalSection(CRITICAL_SECTION *criticlaSection);
+	void setCriticalSection(CRITICAL_SECTION *criticlaSection);
 
-	void							startRecevingThread();
-	void							closeRecevingThread();
+	void startRecevingThread();
+	void closeRecevingThread();
 
-	SOCKET							getRecevingSocket(int port, int* sockNum, vector<SOCKET> * sockArray, vector<WSAEVENT> * eventArray);
+	SOCKET getRecevingSocket(int port, int* sockNum, vector<SOCKET> * sockArray, vector<WSAEVENT> * eventArray);
 
-	void							CloseProc(int idx, int * totalSocket, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
-	PDD_NODE						ReadProc(int idx, int * strLen, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray, sockaddr_in * addr);
-	void							AcceptProc(int idx, int *totalSocket, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
-	void							savePacketToDeque(CRITICAL_SECTION * cs, deque<pair<IN_ADDR, PDD_NODE>>	* dq, PDD_NODE * pn, sockaddr_in addr);
-	void						 	getRecevingDEQUE(deque<pair<IN_ADDR, PDD_NODE>> ** dq);
-	void							sendPacket(char * TargetAddress, const char * Datagram, int SizeOfDatagram, int port);
+	void CloseProc(int idx, int * totalSocket, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
+	PDD_NODE ReadProc(int idx, int * strLen, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray, sockaddr_in * addr);
+	void AcceptProc(int idx, int *totalSocket, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
+	void savePacketToDeque(CRITICAL_SECTION * cs, deque<pair<IN_ADDR, PDD_NODE>>	* dq, PDD_NODE * pn, sockaddr_in addr);
+	void getRecevingDEQUE(deque<pair<IN_ADDR, PDD_NODE>> ** dq);
+	void sendPacket(char * TargetAddress, const char * Datagram, int SizeOfDatagram, int port);
 
 private:
-	HANDLE							recvThread;
+	HANDLE recvThread;
+	SOCKET createSocket();
 
-	void							setUIView();
-	SOCKET 							createSocket();
+	void setSocketTargetAddress(SOCKET * s, char * TargetAddress, int port);
+	void bindingSocket(SOCKET servSocket, int PORT);
+	static void linkingEvents(SOCKET servSock, int* sockNum, vector<SOCKET> * sockArray, vector<WSAEVENT> * eventArray);
 
-	void							setSocketTargetAddress(SOCKET * s, char * TargetAddress, int port);
-	void							bindingSocket(SOCKET servSocket, int PORT);
-	static void 					linkingEvents(SOCKET servSock, int* sockNum, vector<SOCKET> * sockArray, vector<WSAEVENT> * eventArray);
-	
-	//receving
-	bool							isWsaWaitERROR();
-
-	bool							acceptConnection();
-	bool							closeConnection();
-
-	void							insertSocketEvent(int * idx, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray, SOCKET s, WSAEVENT Event);
-	void							deleteSocketEvent(int * idx, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
+	void insertSocketEvent(int * idx, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray, SOCKET s, WSAEVENT Event);
+	void deleteSocketEvent(int * idx, vector<SOCKET> * SocketArray, vector<WSAEVENT> * EventArray);
 };
 
