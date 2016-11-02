8a2447ef7e5eeefa6b2686e84bb41bf75dccd18a
오준영 <plus4070@gmail.com>
2016-11-02 23:02:04 +0900

    MessageHandler.h 들여쓰기 정리

--- #3/TNS_Terminal/TNS_Terminal/MessageHandler.h
+++ #3/TNS_Terminal/TNS_Terminal/MessageHandler.h
@@ -8,14 +8,14 @@ public:
 	~MessageHandler();
 
 	//recive data process
-	void			setPDDNode(PDD_NODE *PN);
-	void			addDataToNode(PPDD_NODE PN, PDD_DATA PD);
+	void setPDDNode(PDD_NODE *PN);
+	void addDataToNode(PPDD_NODE PN, PDD_DATA PD);
 
-	bool			isPacketAvailable(PDD_NODE *PN);
-	void			setParticipantNumber(PDD_NODE *PN, int num);
-	void			setMessageTypeTopicNotExist(PDD_NODE *PN);
-	void			setMessageTypeProcessDone(PDD_NODE *PN);
-	void			setMessageType(PDD_NODE *PN, int type);
-	void			copyDatagram(PDD_NODE * oldNode, PDD_NODE * newNode);
+	bool isPacketAvailable(PDD_NODE *PN);
+	void setParticipantNumber(PDD_NODE *PN, int num);
+	void setMessageTypeTopicNotExist(PDD_NODE *PN);
+	void setMessageTypeProcessDone(PDD_NODE *PN);
+	void setMessageType(PDD_NODE *PN, int type);
+	void copyDatagram(PDD_NODE * oldNode, PDD_NODE * newNode);
 };
 
